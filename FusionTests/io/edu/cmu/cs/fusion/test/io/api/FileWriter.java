798093b4190f1cc9a3c7211d5d524386a400dd2d
ciera.christopher <ciera.christopher@30c243e2-1a5b-11de-bcda-2bb9902f8029>
2009-10-26 17:03:44 +0000

    Some new APIs after talks with Michael Pradel

---
+++ FusionTests/io/edu/cmu/cs/fusion/test/io/api/FileWriter.java
@@ -0,0 +1,31 @@
+package edu.cmu.cs.fusion.test.io.api;
+
+import edu.cmu.cs.fusion.annot.Constraint;
+import edu.cmu.cs.fusion.annot.Constraints;
+import edu.cmu.cs.fusion.annot.Relation.Effect;
+import edu.cmu.cs.fusion.test.io.relations.Writeable;
+import edu.cmu.cs.fusion.test.io.relations.Closed;
+
+@Constraints({
+@Constraint(
+		op = "FileWriter.write(String) : void",
+		trigger = "TRUE",
+		requires = "Writeable(target)",
+		effects = {}
+),
+@Constraint(
+		op = "EOM",
+		trigger = "TRUE",
+		requires = "Closed(target)",
+		effects = {}
+)})
+public class FileWriter {
+	@Writeable({"target"})
+	public FileWriter() {}
+	
+	public void write(String str) {}
+	
+	@Closed({"target"})
+	@Writeable(value = {"target"}, effect = Effect.REMOVE)
+	public void close() {}
+}
---
+++ FusionTests/iterator/edu/cmu/cs/fusion/test/iterator/api/Collection.java
@@ -0,0 +1,35 @@
+package edu.cmu.cs.fusion.test.iterator.api;
+
+import edu.cmu.cs.fusion.annot.Constraint;
+import edu.cmu.cs.fusion.annot.Relation.Effect;
+import edu.cmu.cs.fusion.test.iterator.relations.Iterator;
+import edu.cmu.cs.fusion.test.iterator.relations.Item;
+
+@Constraint(
+		op = "Iterator.next() : Object",
+		trigger = "TRUE",
+		requires = "HasNext(target) AND Iterator(iterator, coll)",
+		effects = {"!HasNext(target)", "Item(result, coll)"}
+)
+public class Collection {
+	public Collection() {}
+	
+	@Iterator({"result", "target"})
+	public Iterator iterator() {return null;
+	}
+	
+	@Item({"item", "target"})
+	@Iterator(value = {"*", "target"}, effect = Effect.REMOVE)
+	public void add(Object item) {}
+	
+	@Item(value = {"item", "target"}, effect = Effect.TEST, test = "result")
+	public boolean contains(Object item) {return false;}
+	
+	@Item(value = {"*", "target"}, effect = Effect.REMOVE)
+	@Iterator(value = {"*", "target"}, effect = Effect.REMOVE)
+	public void clear() {}
+
+
+	@Item({"result", "target"})
+	public Object getItem(int item) {return null;}
+}
