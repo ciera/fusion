e5a0812862a7a53588f2c182ed73cd2a0eb22b69
ciera.christopher <ciera.christopher@30c243e2-1a5b-11de-bcda-2bb9902f8029>
2010-01-18 18:27:09 +0000

    Fixed the dictenum problems

--- trunk/FusionTests/iterator/edu/cmu/cs/fusion/test/iterator/relations/Item.java
+++ trunk/FusionTests/iterator/edu/cmu/cs/fusion/test/iterator/relations/Item.java
@@ -4,7 +4,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import edu.cmu.cs.fusion.test.iterator.api.*;
+import edu.cmu.cs.fusion.test.iterator.api.*; 
 
 import edu.cmu.cs.fusion.annot.Relation;
 import edu.cmu.cs.fusion.test.iterator.api.Iterator;
