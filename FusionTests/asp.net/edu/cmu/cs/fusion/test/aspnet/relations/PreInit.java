fb7a982b93b821b0a65bd8f9bb48738111dc0288
ciera.christopher <ciera.christopher@30c243e2-1a5b-11de-bcda-2bb9902f8029>
2010-06-14 14:44:42 +0000

    Added Preinit callback

---
+++ FusionTests/asp.net/edu/cmu/cs/fusion/test/aspnet/relations/PreInit.java
@@ -0,0 +1,19 @@
+package edu.cmu.cs.fusion.test.aspnet.relations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import edu.cmu.cs.fusion.annot.Relation;
+import edu.cmu.cs.fusion.test.aspnet.api.DropDownList;
+import edu.cmu.cs.fusion.test.aspnet.api.Page;
+
+@Relation({Page.class})
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD})
+public @interface PreInit {
+	public String[] value();
+	public Relation.Effect effect() default Relation.Effect.ADD;
+	public String test() default "";
+}
