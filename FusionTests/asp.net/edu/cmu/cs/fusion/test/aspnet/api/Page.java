e9fded428d31dc51ccc0c47a7d04392439cf8d53
ciera.christopher <ciera.christopher@30c243e2-1a5b-11de-bcda-2bb9902f8029>
2010-06-14 14:43:17 +0000

    Added callbacks.

--- FusionTests/asp.net/edu/cmu/cs/fusion/test/aspnet/api/Page.java
+++ FusionTests/asp.net/edu/cmu/cs/fusion/test/aspnet/api/Page.java
@@ -1,11 +1,35 @@
 package edu.cmu.cs.fusion.test.aspnet.api;
 
+import edu.cmu.cs.fusion.annot.Callback;
 import edu.cmu.cs.fusion.annot.Constraint;
 import edu.cmu.cs.fusion.annot.Constraints;
 import edu.cmu.cs.fusion.annot.Infer;
 import edu.cmu.cs.fusion.test.aspnet.relations.*;
 
 public class Page extends Control {
+	
+	@Callback(name = "PreInit")
+	public void preInit() {
+	}
+	
+	@Callback(name = "Init")
+	public void init() {
+	}
+	
+	@Callback(name = "PreLoad")
+	public void preLoad() {
+	}
+
+	@Callback(name = "Load")
+	public void load() {
+		
+	}
+	
+	public void render() {
+	}
+
+	public void close() {
+	}
 
 	@PageRequest({"result", "target"})
 	public Request getRequest() {
