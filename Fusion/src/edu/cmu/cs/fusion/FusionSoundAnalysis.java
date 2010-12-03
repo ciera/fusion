5fe025c549a67cabfd4d2e163b2e6fda919c9179
ciera.christopher <ciera.christopher@30c243e2-1a5b-11de-bcda-2bb9902f8029>
2010-12-03 16:03:18 +0000

    Added a Must alias analysis

--- Fusion/src/edu/cmu/cs/fusion/FusionSoundAnalysis.java
+++ Fusion/src/edu/cmu/cs/fusion/FusionSoundAnalysis.java
@@ -1,6 +1,13 @@
 package edu.cmu.cs.fusion;
 
-public class FusionSoundAnalysis extends FusionAnalysis {
+import edu.cmu.cs.crystal.flow.ILatticeOperations;
+import edu.cmu.cs.crystal.tac.AbstractTACBranchSensitiveTransferFunction;
+import edu.cmu.cs.crystal.util.TypeHierarchy;
+import edu.cmu.cs.fusion.alias.MayPointsToTransferFunctions;
+import edu.cmu.cs.fusion.alias.PointsToAliasContext;
+import edu.cmu.cs.fusion.alias.PointsToLatticeOps;
+
+public class FusionSoundAnalysis extends FusionAnalysis<PointsToAliasContext> {
 
 	public FusionSoundAnalysis() {
 		super(Variant.SOUND_VARIANT);
@@ -8,5 +15,13 @@ public class FusionSoundAnalysis extends FusionAnalysis {
 
 	@Override
 	public String getName() {return "FusionSound";}
+
+	public AbstractTACBranchSensitiveTransferFunction<PointsToAliasContext> getAliasTransferFunction(DeclarativeRetriever retriever, TypeHierarchy types) {
+		return new MayPointsToTransferFunctions(retriever, types);
+	}
+
+	public ILatticeOperations<PointsToAliasContext> getAliasLatticeOps(TypeHierarchy types) {
+		return new PointsToLatticeOps(types);
+	}
 
 }
--- Fusion/test/edu/cmu/cs/fusion/test/StubFusionAnalysis.java
+++ Fusion/test/edu/cmu/cs/fusion/test/StubFusionAnalysis.java
@@ -1,11 +1,25 @@
 package edu.cmu.cs.fusion.test;
 
+import edu.cmu.cs.crystal.flow.ILatticeOperations;
+import edu.cmu.cs.crystal.tac.AbstractTACBranchSensitiveTransferFunction;
+import edu.cmu.cs.crystal.util.TypeHierarchy;
+import edu.cmu.cs.fusion.DeclarativeRetriever;
 import edu.cmu.cs.fusion.FusionAnalysis;
+import edu.cmu.cs.fusion.alias.MayPointsToTransferFunctions;
+import edu.cmu.cs.fusion.alias.PointsToAliasContext;
+import edu.cmu.cs.fusion.alias.PointsToLatticeOps;
 
-public class StubFusionAnalysis extends FusionAnalysis {
+public class StubFusionAnalysis extends FusionAnalysis<PointsToAliasContext> {
 
 	public StubFusionAnalysis() {
 		super(null);
 	}
 	
+	public AbstractTACBranchSensitiveTransferFunction<PointsToAliasContext> getAliasTransferFunction(DeclarativeRetriever retriever, TypeHierarchy types) {
+		return new MayPointsToTransferFunctions(retriever, types);
+	}
+
+	public ILatticeOperations<PointsToAliasContext> getAliasLatticeOps(TypeHierarchy types) {
+		return new PointsToLatticeOps(types);
+	}
 }
